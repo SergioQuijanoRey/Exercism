2edb05d10e5c85a95c1a716b4779a2e2daba7bd1
Sergio Quijano Rey <sergiquijano@gmail.com>
2020-09-10 12:06:19 +0200

    feat go: hago unas modificaciones al problema de difference_of_squares

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -3,18 +3,16 @@ package diffsquares
 
 // SquareOfSum calculates the square of the natural series up to n
 func SquareOfSum(n int) int {
-	sum := 0.5 * float64(n) * float64(n+1)
-	return int(sum * sum)
+	sum := (n * (n + 1)) / 2
+	return sum * sum
 }
 
 // SquareOfSum calculates the natural squares series up to n
 func SumOfSquares(n int) int {
-	// The factor is an aproximation of the value 1 / 6
-	var factor float64 = 0.1666666666667
-	return int(factor * float64(n) * float64(n+1) * float64(2*n+1))
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
-// DDifference calculates the difference of the square of natural series up to n
+// Difference calculates the difference of the square of natural series up to n
 // minus the natural squares series up to n
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
